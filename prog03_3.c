ab440b8cce04135572e3b16f2d9626d461eedb2b
oraku3 <57931077+oraku3@users.noreply.github.com>
2019-11-19 13:35:05 +0900

    Create prog03_3.c
    InputShape func is here.

---
+++ prog03_3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#if defined SWITCH
+typedef struct{
+  double x,y;
+   } XY;
+
+typedef struct{
+  XY temp[3];
+} TRY;
+TRY TRY1,TRY2;
+
+void InputShape(TRY);
+int main(){
+  TRY1 = InputShape(TRY1);
+  TRY2 = InputShape(TRY2);
+  return 0;
+}
+#endif
+void InputShape(TRY try){
+  printf("vertex 1: ");
+  scanf("%lf%lf",&try.temp[0].x,&try.temp[0].y);
+  printf("vertex 2: ");
+  scanf("%lf%lf",&try.temp[1].x,&try.temp[1].y);
+  printf("vertex 3: ");
+  scanf("%lf%lf",&try.temp[2].x,&try.temp[2].y);
+  return try;
+}
