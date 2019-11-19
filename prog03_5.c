c848184bdc27ef603609ea8e590ba1c7836baa9d
oraku3 <57931077+oraku3@users.noreply.github.com>
2019-11-19 13:20:52 +0900

    Update prog03_5.c
    GetArea func is here.

--- prog03_5.c
+++ prog03_5.c
@@ -23,22 +23,16 @@ TRY InputShape(TRY try){
   scanf("%lf%lf",&try.temp[1].x,&try.temp[1].y);
   printf("vertex 3: ");
   scanf("%lf%lf",&try.temp[2].x,&try.temp[2].y);
-  for(int i = 0; i < 3; i++){
-    printf("\tV%d: (%f, %f)\n",i+1,try.temp[i].x,try.temp[i].y);
-  }
   return try;
 }
 
 #endif
 double GetArea(TRY try){
 double area,len01,len12,len02,s;
-printf("%f\n",try.temp[0].x);
 len01 = sqrt((try.temp[0].x - try.temp[1].x)*(try.temp[0].x - try.temp[1].x) + (try.temp[0].y - try.temp[1].y)*(try.temp[0].y - try.temp[1].y));
 len12 = sqrt((try.temp[1].x - try.temp[2].x)*(try.temp[1].x - try.temp[2].x) + (try.temp[1].y - try.temp[2].y)*(try.temp[1].y - try.temp[2].y));  
 len02 = sqrt((try.temp[0].x - try.temp[2].x)*(try.temp[0].x - try.temp[2].x) + (try.temp[0].y - try.temp[2].y)*(try.temp[0].y - try.temp[2].y)); 
 s = (len01 + len12 + len02)/2;
 area = sqrt(s*(s - len01)*(s - len12)*(s - len02));
-printf("len01 = %f\nlen12 = %f\nlen02 = %f\n",len01,len12,len02);
-printf("area = %f\n",area);
 return area;
 }
